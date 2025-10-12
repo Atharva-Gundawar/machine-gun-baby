cdb5002286c9983500c02bf1222e7899eeac9e0d
diego caples <diegocaples@gmail.com>
2025-10-12 06:36:39 +0000

    full rank training multigpu

---
+++ diffusiontraining/DiffSynth-Studio/diego/train_full_multi_gpu.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export NCCL_P2P_DISABLE=1
+export NCCL_SHM_DISABLE=1
+export NCCL_IB_DISABLE=1
+export NCCL_DEBUG=INFO
+export ACCELERATE_LOG_WITH=wandb
+export WANDB_PROJECT=pendulumdreamer
+
+accelerate launch --num_processes=4 train.py \
+  --dataset_base_path data/examples/videotrain \
+  --dataset_metadata_path data/examples/videotrain/metadata.csv \
+  --height 512 \
+  --width 512 \
+  --num_frames 97 \
+  --dataset_repeat 10 \
+  --model_id_with_origin_paths "Wan-AI/Wan2.2-TI2V-5B:diffusion_pytorch_model*.safetensors,Wan-AI/Wan2.2-TI2V-5B:models_t5_umt5-xxl-enc-bf16.pth,Wan-AI/Wan2.2-TI2V-5B:Wan2.2_VAE.pth" \
+  --learning_rate 1e-4 \
+  --num_epochs 1 \
+  --remove_prefix_in_ckpt "pipe.dit." \
+  --trainable_models "dit" \
+  --output_path "./models/train/Wan2.2-TI2V-5B_full" \
+  --extra_inputs "input_image"
